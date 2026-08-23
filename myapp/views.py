936af51744b56fc9002d38ffed09b6a593911ffa
Nada Hashem <Nooody2005std@gmail.com>
2026-08-24 01:00:44 +0300

    Writing another View

--- myapp/views.py
+++ myapp/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 
 def index(request):
     return  HttpResponse("Hello World")
+
+def item(request):
+    return HttpResponse("<h1>This is the item view</h1>")
+
