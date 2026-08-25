f9b6caec772d10c419047b5154f3acfb356e708f
Nada Hashem <Nooody2005std@gmail.com>
2026-08-25 03:39:17 +0300

    removing hardcoded URLS

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index),
+    path('',views.index,name='index'),
     path('item/',views.item),
-    path('<int:id>/',views.detail),
+    path('<int:id>/',views.detail,name='detail'),
 ]
