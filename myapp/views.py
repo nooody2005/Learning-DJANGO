17db8446d94ccc4f88690c4d0500f14ba64409b3
Nada Hashem <Nooody2005std@gmail.com>
2026-08-25 03:02:07 +0300

    Passing the context to templates

--- myapp/views.py
+++ myapp/views.py
@@ -5,9 +5,18 @@ from .models import Item
 # Create your views here.
 
 def index(request):
+    # Getting items from database
     item_list = Item.objects.all()
+    # Creating context
+    context = {
+        'item_list':item_list
+    }
+    # Passing the context object to the render method along with the template
+    return render(request,"myapp/index.html",context)
+
+
     # return  HttpResponse(item_list)
-    return render(request,"myapp/index.html")
+    
 
 def item(request):
     return HttpResponse("<h1>This is the item view</h1>")
