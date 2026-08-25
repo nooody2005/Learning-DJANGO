e62c2b811a63afa588c9131f9951657ccc50526b
Nada Hashem <Nooody2005std@gmail.com>
2026-08-26 01:26:33 +0300

    Delete functionality

--- myapp/views.py
+++ myapp/views.py
@@ -52,3 +52,23 @@ def update_item(request,id):
     }
     return render(request,'myapp/item-form.html',context)
 
+
+def update_item(request,id):
+    item = Item.objects.get(id=id)
+    form = ItemForm(request.POST or None, instance= item)
+    if form.is_valid():
+        form.save()
+        return redirect('myapp:index')
+    context = {
+        'form' : form
+    }
+    return render(request,'myapp/item-form.html',context)
+
+
+def delete_item(request,id):
+    item = Item.objects.get(id=id)
+    if request.method == "POST":
+        item.delete()
+        return redirect('myapp:index')
+    return render(request,'myapp/item-delete.html')
+
