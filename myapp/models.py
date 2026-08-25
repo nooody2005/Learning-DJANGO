32309d2420f51f7a36ae473eb1b213a25cb706a8
Nada Hashem <Nooody2005std@gmail.com>
2026-08-25 20:17:41 +0300

    Adding image Fields

--- myapp/models.py
+++ myapp/models.py
@@ -9,3 +9,4 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField()
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500,default='https://p.kindpng.com/picc/s/79-798754_hoteles-y-centros-vacacionales-dish-placeholder-hd-png.png')
