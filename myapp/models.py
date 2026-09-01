470e52545e70150b701158ce6b0c67656e3ddd95
Nada Hashem <Nooody2005std@gmail.com>
2026-09-01 14:50:58 +0300

    adding foreign key for food item from uers model

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Item(models.Model):
@@ -8,8 +9,10 @@ class Item(models.Model):
         return self.item_name
 
     def get_absolute_url(self):
-        return reverse()
+        return reverse('myapp:index')
+    
 
+    user_name = models.ForeignKey(User,on_delete=models.CASCADE,default=1)
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField()
     item_price = models.IntegerField()
