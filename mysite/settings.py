8fc46abd618995d51ea4998ff31a455d37662e7d
Nada Hashem <Nooody2005std@gmail.com>
2026-09-08 02:42:13 +0300

    setting up POSTGRE data base

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,10 +12,13 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
@@ -76,12 +79,35 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+# ====================================================
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # if work in postgre
+        'ENGINE': 'django.db.backends.postgresql',
+
+        # # if work in myql 
+        # 'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT','5432'),
+        'COMN_MAX_AGE':60,
+        'OPTIONS':{
+
+        },
     }
 }
+# =======================================================
+
 
 
 # Password validation
