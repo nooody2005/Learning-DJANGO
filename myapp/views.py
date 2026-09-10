ad01ac4938d25c33d5c9260f743e70de9f80f0f9
Nada Hashem <Nooody2005std@gmail.com>
2026-09-10 22:41:23 +0300

    Optimizing Queries

--- myapp/views.py
+++ myapp/views.py
@@ -104,3 +104,18 @@ def get_queryset(self):
 class ItemDelete(DeleteView):
     model= Item
     success_url = reverse_lazy("myapp:index")
+
+
+
+
+
+def get_objects(request):
+    for item in Item.objects.all():
+        print(item.item_name)
+
+
+
+def get_objects_optimized(request):
+    items = Item.objects.only('item_name')
+    for item in items:
+        print(item.item_name)
