e0dd5eb482912ce6a0bb971363fb7d077bfd8b05
Nada Hashem <Nooody2005std@gmail.com>
2026-09-22 23:09:56 +0300

    Caching By Headers

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,7 +7,7 @@ app_name='myapp'
 
 urlpatterns = [
     # path('',views.IndexClassView.as_view(),name='index'),
-    path('',cache_page(60 * 15)(views.index) ,name='index'),
+    path('',views.index ,name='index'),
     # path('item/',views.item),
     path('<int:pk>/',views.FoodDetail.as_view(),name='detail'),
     path('add/',views.ItemCreateView.as_view(),name='create_item'),
--- myapp/views.py
+++ myapp/views.py
@@ -9,11 +9,14 @@ from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from django.urls import reverse_lazy
 from django.core.paginator import Paginator 
 from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers
+
 
 # Create your views here.
 
 # @login_required
-# @cache_page(60 * 15)
+@cache_page(60 * 15)
+@vary_on_headers("User-Agent")
 def index(request):
     # Getting items from database
     item_list = Item.objects.all()
