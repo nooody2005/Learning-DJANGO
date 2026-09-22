e833af766a58d4c4275cad410e6d9b057f8a8bf6
Nada Hashem <Nooody2005std@gmail.com>
2026-09-22 22:56:31 +0300

    Url pattern Caching

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
+from django.views.decorators.cache import cache_page
+
 
 app_name='myapp'
 
 urlpatterns = [
     # path('',views.IndexClassView.as_view(),name='index'),
-    path('',views.index,name='index'),
+    path('',cache_page(60 * 15)(views.index) ,name='index'),
     # path('item/',views.item),
     path('<int:pk>/',views.FoodDetail.as_view(),name='detail'),
     path('add/',views.ItemCreateView.as_view(),name='create_item'),
